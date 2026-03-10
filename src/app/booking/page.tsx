3b0fcc3e87ddeff89e1a08fd0d3670e531d4092b
p0r9 <palathip.opor@gmail.com>
2026-03-10 17:04:40 +0700

    taiwind prop layout

---
+++ src/app/booking/page.tsx
@@ -0,0 +1,13 @@
+export const metadata = {
+  title: "Venue Booking",
+};
+
+export default function Booking(){
+    return(
+        <main className="bg-white text-black min-h-screen p-5">
+            <h1 className="text-3xl font-bold">
+                Venue Booking
+            </h1>
+        </main>
+    );
+}
---
+++ src/components/Card.tsx
@@ -0,0 +1,26 @@
+import styles from "./card.module.css"
+import Image from "next/image";
+
+export default function Card({venueName,imgSrc}:{venueName:string,imgSrc:string}) {
+  return (
+    <div className="w-1/5 h-[300px] rounded-lg shadow-lg bg-white">
+      
+      <div className="w-full h-[70%] relative rounded-t-lg overflow-hidden">
+        <Image
+          src={imgSrc}
+          alt="venue image"
+          fill
+          className="object-cover"
+        />
+      </div>
+
+      <div className="w-full h-[30%] p-2 text-black">
+        <h3 className="font-semibold">{venueName}</h3>
+        <p className="text-sm text-black">
+          A luxurious venue perfect for weddings, corporate events.
+        </p>
+      </div>
+
+    </div>
+  );
+}
